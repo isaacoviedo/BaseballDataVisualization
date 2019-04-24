602f302e1b724bf5c41444e7db3d5f3d09c1b6c1
isaacoviedo <isaacoviedo@tamu.edu>
2019-04-24 01:52:26 -0500

    mongo running on atlas

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def profile_team(tname):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
--- db.py
+++ db.py
@@ -6,7 +6,8 @@ import sys
 # connects to the baseball db
 def connect():
 
-    client = pymongo.MongoClient("mongodb://localhost:27017")
+    url = os.environ.get("MONGO_SERVER")
+    client = pymongo.MongoClient(url)
 
     return client['baseballdb']
 
@@ -17,6 +18,9 @@ def colls_create():
     conn = connect()
     colls = conn.list_collection_names()
 
+    USER = os.environ.get("MONGO_SERVER_USER")
+    PASS = os.environ.get("MONGO_SERVER_PASS")
+
     for filename in os.listdir(PATH):
         
         collname, _ = filename.split('.')
@@ -25,7 +29,11 @@ def colls_create():
         # don't want to have to import a collection twice
         if filename.endswith(".csv") and (collname not in colls):
             print("Importing: {}".format(filename))
-            cmd = "mongoimport -d baseballdb -c {} --type csv --file {}{} --headerline".format(collname,PATH,filename)
+            
+            if "atlas" in sys.argv:
+                cmd = 'mongoimport --host baseballdb-shard-0/baseballdb-shard-00-00-d4jyf.mongodb.net:27017,baseballdb-shard-00-01-d4jyf.mongodb.net:27017,baseballdb-shard-00-02-d4jyf.mongodb.net:27017 --ssl --username {} --password {} --authenticationDatabase admin --db baseballdb --collection {} --type csv --file {}{} --headerline'.format(USER,PASS,collname,PATH,filename)
+            else:
+                cmd = "mongoimport -d baseballdb -c {} --type csv --file {}{} --headerline".format(collname,PATH,filename)
             os.system(cmd)
     
     return
